babc8cf0a6cdc62b6ea3a941bf2ffef578b93389
theprashantchandra <97657867+theprashantchandra@users.noreply.github.com>
2022-09-06 14:48:23 +0530

    first

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Prashant\\OneDrive\\Documents\\GitHub\\Personal.github.io\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Prashant\\OneDrive\\Documents\\GitHub\\Personal.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Prashant\\OneDrive\\Documents\\GitHub\\Personal.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Prashant\\OneDrive\\Documents\\GitHub\\Personal.github.io\\src\\css\\custom.css'),
+];
